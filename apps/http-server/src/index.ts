c8fde068ef8679c0518485a6298adc15072d7547
Rohit Kumar <rohitnarnolia88@gmail.com>
2025-07-22 12:00:58 +0530

    init simple http server

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import { client } from "@repo/db/client"
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello")
+})
+
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await client.user.create({ data: { username, password } })
+
+    res.json({
+        message: "Signup successful",
+        id: user.id
+    })
+})
+
+app.listen(3000)
